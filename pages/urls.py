434c96ff04059bd01ceda14a1701ac1c07c67944
Eduardo Albuquerque Ribeiro <eduardoribeiro.advg@gmail.com>
2025-05-26 23:26:37 -0300

    Feat: Added new entries for about, projects, contact, altered a few information on the website

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import MainPage, AboutView
+from .views import MainPage, AboutView, ProjectsView, ContactView
 
 urlpatterns = [
     path('', MainPage.as_view(), name='index'),
     path('about/', AboutView.as_view(), name='about'),
+    path('projects/', ProjectsView.as_view(), name='projects'),
+    path('contact/', ContactView.as_view(), name='contact'),
     #path('endereço/', MinhaView.as_view(), name='nome-da-url'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,13 @@
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import TemplateView
 
 class MainPage(TemplateView):
     template_name = 'pages/index.html'
 
 class AboutView(TemplateView):
     template_name = 'pages/about.html'
+
+class ProjectsView(TemplateView):
+    template_name = 'pages/projects.html'
+
+class ContactView(TemplateView):
+    template_name = 'pages/contact.html'
