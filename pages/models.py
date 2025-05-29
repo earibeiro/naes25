97bae72ccfb3cdc68ed78bd8db2e54ac833226dd
Eduardo Albuquerque Ribeiro <eduardoribeiro.advg@gmail.com>
2025-05-29 17:58:44 -0300

    Feat: Added forms, fixed the about section, created admin and superuser

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class State(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    abbreviation = models.CharField(max_length=2, unique=True)
+
+    def __str__(self):
+        return self.abbreviation
+    
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    state = models.ForeignKey(State, on_delete=models.CASCADE, related_name='cities')
+
+    class Meta:
+        unique_together = ('name', 'state')
+
+    def __str__(self):
+        return f"{self.name}, {self.state.abbreviation}"
+    
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11, unique=True)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='pessoas_fisicas')
+
+    def __str__(self):
+        return f"{self.name} ({self.cpf})"
+    
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    cnpj = models.CharField(max_length=14, unique=True)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='pessoas_juridicas')
+
+    def __str__(self):
+        return f"{self.name} ({self.cnpj})"
+    
+
+
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,7 @@
 from django.views.generic import TemplateView
+from .models import State, City, Person, Company
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 class MainPage(TemplateView):
     template_name = 'pages/index.html'
@@ -11,3 +14,71 @@ class ProjectsView(TemplateView):
 
 class ContactView(TemplateView):
     template_name = 'pages/contact.html'
+
+class StateCreateView(CreateView):
+    model = State
+    fields = ['name', 'abbreviation']
+    template_name = 'pages/state_form.html'
+    success_url = reverse_lazy('index')
+
+class CityCreateView(CreateView):
+    model = City
+    fields = ['name', 'state']
+    template_name = 'pages/city_form.html'
+    success_url = reverse_lazy('index')
+
+class PersonCreateView(CreateView):
+    model = Person
+    fields = ['name', 'cpf', 'email', 'phone', 'city']
+    template_name = 'pages/person_form.html'
+    success_url = reverse_lazy('index')
+
+class CompanyCreateView(CreateView):
+    model = Company
+    fields = ['name', 'cnpj', 'email', 'phone', 'city']
+    template_name = 'pages/company_form.html'
+    success_url = reverse_lazy('index')
+
+class StateUpdateView(UpdateView):
+    model = State
+    fields = ['name', 'abbreviation']
+    template_name = 'pages/state_form.html'
+    success_url = reverse_lazy('index')
+
+class CityUpdateView(UpdateView):
+    model = City
+    fields = ['name', 'state']
+    template_name = 'pages/city_form.html'
+    success_url = reverse_lazy('index')
+
+class PersonUpdateView(UpdateView):
+    model = Person
+    fields = ['name', 'cpf', 'email', 'phone', 'city']
+    template_name = 'pages/person_form.html'
+    success_url = reverse_lazy('index')
+
+class CompanyUpdateView(UpdateView):
+    model = Company
+    fields = ['name', 'cnpj', 'email', 'phone', 'city']
+    template_name = 'pages/company_form.html'
+    success_url = reverse_lazy('index')
+
+class StateDeleteView(DeleteView):
+    model = State
+    template_name = 'pages/state_confirm_delete.html'
+    success_url = reverse_lazy('index')
+
+class CityDeleteView(DeleteView):
+    model = City
+    template_name = 'pages/city_confirm_delete.html'
+    success_url = reverse_lazy('index')
+
+class PersonDeleteView(DeleteView):
+    model = Person
+    template_name = 'pages/person_confirm_delete.html'
+    success_url = reverse_lazy('index')
+
+class CompanyDeleteView(DeleteView):
+    model = Company
+    template_name = 'pages/company_confirm_delete.html'
+    success_url = reverse_lazy('index')
